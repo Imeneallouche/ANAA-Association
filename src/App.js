04077924fd749a488e43f6dc40a7e2e1d1ca3326
im3ne <im3ne@localhost.localdomain>
2025-06-13 12:30:26 +0100

    initial home page has been added

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Home from "./pages/home";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App w-screen h-screen">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
